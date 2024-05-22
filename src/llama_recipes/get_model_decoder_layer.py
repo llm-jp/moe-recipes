c60d9919266cd0ffaa9354229a3a8e3baa728594
kazuki <kaz.tokyo.tech20@gmail.com>
2024-05-22 22:49:30 +0900

    skip: qwen support (no tested)

--- src/llama_recipes/get_model_decoder_layer.py
+++ src/llama_recipes/get_model_decoder_layer.py
@@ -1,10 +1,13 @@
 from transformers.models.mixtral.modeling_mixtral import MixtralDecoderLayer
+from transformers.models.qwen2_moe.modeling_qwen2_moe import Qwen2MoeDecoderLayer
 
 
 def get_model_decoder_layer(
     model_name: str,
-) -> type[MixtralDecoderLayer]:
+) -> type[MixtralDecoderLayer] | type[Qwen2MoeDecoderLayer]:
     if "Mixtral" in model_name:
         return MixtralDecoderLayer
+    elif "Qwen" in model_name:
+        return Qwen2MoeDecoderLayer
     else:
         raise NotImplementedError(f"{model_name}: this model decoder layer is not implemented.")
--- src/llama_recipes/get_models.py
+++ src/llama_recipes/get_models.py
@@ -1,5 +1,6 @@
 from transformers import (
     MixtralForCausalLM,
+    Qwen2MoeForCausalLM,
     MixtralConfig,
     AutoModelForCausalLM,
 )
@@ -10,7 +11,7 @@ from transformers.integrations import is_deepspeed_zero3_enabled
 
 def get_model(
     model_name: str, use_cache: bool = False
-) -> MixtralForCausalLM | AutoModelForCausalLM:
+) -> MixtralForCausalLM | Qwen2MoeForCausalLM | AutoModelForCausalLM:
     args = get_args()
 
     if "Mixtral" in model_name:
@@ -19,6 +20,18 @@ def get_model(
             attn_implementation="flash_attention_2",
             max_position_embeddings=args.seq_length,
             # ref: https://huggingface.co/mistralai/Mixtral-8x7B-Instruct-v0.1/blob/main/config.json#L19
+            output_router_logits=args.output_router_logits,
+            torch_dtype=torch.bfloat16 if args.bf16 else torch.float16,
+            use_cache=use_cache,
+        )
+
+        return model  # type: ignore
+    elif "Qwen" in model_name:
+        model = Qwen2MoeForCausalLM.from_pretrained(
+            model_name,
+            attn_implementation="flash_attention_2",
+            max_position_embeddings=args.seq_length,
+            # ref: https://huggingface.co/Qwen/Qwen1.5-MoE-A2.7B/blob/main/config.json#L33
             output_router_logits=args.output_router_logits,
             torch_dtype=torch.bfloat16 if args.bf16 else torch.float16,
             use_cache=use_cache,
